3c95abfa65f812719183570b954a51f03aa97000
Adebayo <94082200+Ipsegs@users.noreply.github.com>
2022-05-10 00:09:33 +0100

    testing argo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world"
+    return "Hello guys, welcome"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
